a2bb762dc5debb17872f7a6380010f94922ab8cc
Esteban <esteban98e31@gmail.com>
2023-10-24 17:28:31 -0500

    Protegiendo rutas

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ComunaController;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,5 +28,15 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+require __DIR__.'/auth.php';  
 
-require __DIR__.'/auth.php';
+Route::middleware('auth')->group(function () {
+Route::get('/comunas', [ComunaController::class, 'index'])->name('comunas.index');
+Route::post('/comunas', [ComunaController::class, 'store'])->name('comunas.store');
+Route::get('/comunas/create', [ComunaController::class, 'create'])->name('comunas.create');
+Route::delete('/comunas/{comuna}', [ComunaController::class, 'destroy'])->name('comunas.destroy');
+Route::put('/comunas/{comuna}', [ComunaController::class, 'update'])->name('comunas.update');
+Route::get('/comunas/{comuna}/edit', [ComunaController::class, 'edit'])->name('comunas.edit');
+});
+
+  
